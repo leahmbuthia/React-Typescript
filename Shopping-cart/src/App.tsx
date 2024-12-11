b51138abeccb4f19eb22ad425e64ce613c3410e3
leahmbuthia <leahnyambura682@gmail.com>
2024-12-11 16:19:28 +0300

    Shopping carts are one of the most common things you will build as a web developer, but there is a lot that goes into building a good shopping cart. In this video I will show you how to build a fully functional shopping cart using React, TypeScript, and Bootstrap.

---
+++ Shopping-cart/src/App.tsx
@@ -0,0 +1,26 @@
+
+import { Route, Routes } from 'react-router-dom'
+import { Container } from 'react-bootstrap'
+import Home from './pages/Home';
+import About from './pages/About';
+import { Store } from './pages/Store';
+import { Navbar } from './components/Navbar';
+
+
+
+function App() {
+  
+
+  return (
+    <Container className='mb-4'>
+      <Navbar/>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/store" element={<Store />} />
+        <Route path="/about" element={<About />} />
+      </Routes>
+    </Container>
+  );
+}
+
+export default App
